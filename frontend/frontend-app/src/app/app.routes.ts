e4b0624d53ac3bd89751a3c365d5ce499e83c2b7
khadija <khadijawele8@gmail.com>
2026-05-14 18:39:07 +0000

    feat: member 1 frontend setup finished

---
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Offers } from './pages/offers/offers';
+
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'offres', component: Offers },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+];
---
+++ frontend/frontend-app/src/app/pages/offers/offers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers',
+  imports: [],
+  templateUrl: './offers.html',
+  styleUrl: './offers.scss',
+})
+export class Offers {}
